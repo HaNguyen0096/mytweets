3d05789fecae60c6ccc206d0e9c12fc71da3256d
Ha Nguyen <hanguyen.0096@gmail.com>
2020-08-05 01:29:46 -0400

    adding like field

--- tweets/models.py
+++ tweets/models.py
@@ -4,10 +4,15 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     # a user can own multiple tweets. if a user was deleted, their tweets also get deleted
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
