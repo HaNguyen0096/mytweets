6cbc209571656d9717a6c78c084c41e7887004ad
Ha Nguyen <hanguyen.0096@gmail.com>
2020-08-06 17:16:45 -0400

    add timestamp

--- tweets/models.py
+++ tweets/models.py
@@ -7,6 +7,7 @@ User = settings.AUTH_USER_MODEL
 class TweetLike(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
 
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
@@ -15,6 +16,7 @@ class Tweet(models.Model):
     likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now=True)
 
     # def __str__(self):
     #     return self.content
