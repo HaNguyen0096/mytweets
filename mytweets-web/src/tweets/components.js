5a4e30ed3b7706ad29722090a20ea3c48dcb58d7
Ha Nguyen <hanguyen.0096@gmail.com>
2020-08-14 21:19:32 -0400

    set listview with specific username

--- mytweets-web/src/tweets/components.js
+++ mytweets-web/src/tweets/components.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {apiTweetAction, apiTweetCreate, apiTweetList} from './lookup'
 
 export function TweetsComponent(props) {
+  console.log(props)
     const textAreaRef = React.createRef()
     const [newTweets, setNewTweets] = useState([])
     const handleBackendUpdate = (response, status) => {
