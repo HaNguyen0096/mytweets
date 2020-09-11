bac09cc93ffe804572c006f5c9ce89bc946ea1ed
Ha Nguyen <hanguyen.0096@gmail.com>
2020-09-10 21:20:55 -0400

    handling list view response

--- mytweets-web/src/tweets/list.js
+++ mytweets-web/src/tweets/list.js
@@ -1,22 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState}  from 'react'
 import {apiTweetList} from './lookup'
 import {Tweet} from './detail'
 
 export function TweetsList(props) {
     const [tweetsInit, setTweetsInit] = useState([])
     const [tweets, setTweets] = useState([])
+    const [nextUrl, setNextUrl] = useState(null)
     const [tweetsDidSet, setTweetsDidSet] = useState(false)
-    useEffect(() => {
+    useEffect(()=>{
       const final = [...props.newTweets].concat(tweetsInit)
       if (final.length !== tweets.length) {
-          setTweets(final)
+        setTweets(final)
       }
     }, [props.newTweets, tweets, tweetsInit])
+
     useEffect(() => {
-      if (tweetsDidSet === false) {
+      if (tweetsDidSet === false){
         const handleTweetListLookup = (response, status) => {
-          if (status === 200) {
-            setTweetsInit(response)
+          if (status === 200){
+            setNextUrl(response.next)
+            setTweetsInit(response.results)
             setTweetsDidSet(true)
           } else {
             alert("There was an error")
@@ -26,19 +29,22 @@ export function TweetsList(props) {
       }
     }, [tweetsInit, tweetsDidSet, setTweetsDidSet, props.username])
 
+
     const handleDidRetweet = (newTweet) => {
-      const updateTweetInit = [...tweetsInit]
-      updateTweetInit.unshift(newTweet)
-      setTweetsInit(updateTweetInit)
+      const updateTweetsInit = [...tweetsInit]
+      updateTweetsInit.unshift(newTweet)
+      setTweetsInit(updateTweetsInit)
       const updateFinalTweets = [...tweets]
       updateFinalTweets.unshift(tweets)
       setTweets(updateFinalTweets)
     }
-    return tweets.map((item, index) => {
-      return <Tweet 
+    return <React.Fragment>{tweets.map((item, index)=>{
+      return <Tweet  
         tweet={item} 
-        didRetweet = {handleDidRetweet}
+        didRetweet={handleDidRetweet}
         className='my-5 py-5 border bg-white text-dark' 
-        key={`${index}-{item.id}`}/>
-    })
-}
+        key={`${index}-{item.id}`} />
+    })}
+    { nextUrl !== null && <button className='btn btn-outline-primary'>Load next</button>}
+    </React.Fragment>
+  }
