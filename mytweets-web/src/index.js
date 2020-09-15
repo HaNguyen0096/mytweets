8ce5636b65565c5564331af6f2299269ce1b2d47
Ha Nguyen <hanguyen.0096@gmail.com>
2020-09-14 21:41:42 -0400

    render profile badge component

--- mytweets-web/src/index.js
+++ mytweets-web/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ProfileBadgeComponent} from './profiles'
 import {FeedComponent, TweetsComponent, TweetDetailComponent} from './tweets'
 import * as serviceWorker from './serviceWorker';
 
@@ -28,6 +29,14 @@ tweetDetailElements.forEach(container => {
   ReactDOM.render(
     e(TweetDetailComponent, container.dataset),
     container);
+})
+
+const userProfileBadgeElements = document.querySelectorAll(".mytweet-profile-badge")
+
+userProfileBadgeElements.forEach(container=> {
+    ReactDOM.render(
+        e(ProfileBadgeComponent, container.dataset), 
+        container);
 })
 
 // If you want your app to work offline and load faster, you can change
